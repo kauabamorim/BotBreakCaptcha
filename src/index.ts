809afb4a0c16749c48fda09289f90767c45f79c4
Kauã Berman Amorim <kauabamorim@outlook.com>
2023-08-01 13:38:37 -0300

    Update Remove Max Retries

--- src/index.ts
+++ src/index.ts
@@ -6,82 +6,87 @@ import parse from "node-html-parser";
 const API_KEY = "1d293c30da707756b8d0ca1df2a4b8ef";
 
 const breakCaptcha = async () => {
-  let captchaId = "";
-
-  try {
-    const response = await axios.post("http://2captcha.com/in.php", {
-      key: API_KEY,
-      googlekey: "6LdXo-ISAAAAAFkY4XibHMoIIFk-zIaJ5Gv9mcnQ",
-      method: "userrecaptcha",
-      pageurl:
-        "https://online6.detran.pe.gov.br/ServicosWeb/VeiculoMVC/ConsultaPlaca/ConsultarPlaca",
-    });
-
-    captchaId = response.data.replace("OK|", "");
-    console.log("Resposta da API:", response.data);
-
-    const maxRetries = 5;
-    const timeoutMillis = 25000;
-
-    for (let retry = 1; retry <= maxRetries; retry++) {
-      try {
-        const captcha = await axios.get(
-          `http://2captcha.com/res.php?key=${API_KEY}&action=get&id=${captchaId}`,
-          {
-            timeout: timeoutMillis,
+    let captchaId = "";
+  
+    try {
+      const response = await axios.post("http://2captcha.com/in.php", {
+        key: API_KEY,
+        googlekey: "6LdXo-ISAAAAAFkY4XibHMoIIFk-zIaJ5Gv9mcnQ",
+        method: "userrecaptcha",
+        pageurl:
+          "https://online6.detran.pe.gov.br/ServicosWeb/VeiculoMVC/ConsultaPlaca/ConsultarPlaca",
+      });
+  
+      captchaId = response.data.replace("OK|", "");
+      console.log("Resposta da API:", response.data);
+  
+      const timeoutMillis = 25000;
+  
+      while (true) {
+        try {
+          const captcha = await axios.get(
+            `http://2captcha.com/res.php?key=${API_KEY}&action=get&id=${captchaId}`,
+            {
+              timeout: timeoutMillis,
+            }
+          );
+  
+          const captchaResponse = captcha.data;
+          if (captchaResponse.includes("OK|")) {
+            captchaResponse.replace("OK|", "");
+            return captchaResponse.replace("OK|", "");
+          } else {
+            console.log("Tentativa");
           }
-        );
-
-        const captchaResponse = captcha.data;
-        if (captchaResponse.includes("OK|")) {
-          captchaResponse.replace("OK|", "");
-          return captchaResponse.replace("OK|", "");
-        } else {
-          console.log("Tentativa", retry);
+        } catch (error) {
+          console.error("Erro ao tentar obter o resultado do captcha:", error);
         }
-      } catch (error) {
-        console.error("Erro na tentativa", retry, ":", error);
+        await new Promise((resolve) => setTimeout(resolve, 13000));
       }
-      await new Promise((resolve) => setTimeout(resolve, 13000));
+    } catch (error) {
+      console.error("Erro ao enviar captcha:", error);
+      throw error;
     }
-
-    throw new Error(
-      "Todas as tentativas falharam. Não foi possível obter o resultado do captcha."
-    );
-  } catch (error) {
-    console.error("Erro ao enviar captcha:", error);
-    throw error;
-  }
-};
+  };
 
 const jar = new CookieJar();
 const client = wrapper(
-    axios.create({
-        jar,
-    })
+  axios.create({
+    jar,
+  })
 );
 
 export const bot = async () => {
+  let plate = "PFJ7699";
+
   try {
     const captchaResponse = await breakCaptcha();
 
-    await axios.post(
+    await client.post(
       "https://online6.detran.pe.gov.br/ServicosWeb/VeiculoMVC/ConsultaPlaca/ConsultarPlaca",
       {
-        Placa: "PFJ7699",
+        Placa: plate,
         captcha: captchaResponse,
       }
     );
 
     const response = await client.get(
-        `https://online6.detran.pe.gov.br/ServicosWeb/VeiculoMVC/DetalhamentoDebitos/Detalhamento?Placa=PFJ7699&PlacaOutraUF=N`,
+      `https://online6.detran.pe.gov.br/ServicosWeb/VeiculoMVC/ConsultaPlaca/DetalharDebito?placa=${plate}&PlacaOutraUF=N`,
+      {
+        maxRedirects: 5,
+      }
     );
 
-    const root = parse(response.data);
-    const plate = root.querySelector("#placa");
+    if (response.headers["set-cookie"]) {
+      const setCookieHeaders = response.headers["set-cookie"];
+      setCookieHeaders.forEach((header) => {
+        jar.setCookieSync(header, "https://online6.detran.pe.gov.br");
+      });
+    }
 
-    console.log("Teste ",plate);
-    
+    const root = parse(response.data);
+    const plateInfo = root.querySelector("h2");
+    console.log(plateInfo?.textContent.trim());
   } catch (error) {
     console.log(error);
   }
